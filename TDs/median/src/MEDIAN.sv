bb78b8d0df5f8d62cbe7c1af3270741041eaaa5b
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-12-03 17:41:25 +0100

    Etablissement du signal BYP OK

--- TDs/median/src/MEDIAN.sv
+++ TDs/median/src/MEDIAN.sv
@@ -1,4 +1,4 @@
-module MEDIAN #(parameter SIZE = 8) (input logic [SIZE-1:0] DI,
+module MEDIAN #(parameter SIZE = 8, parameter LENGHT = 9) (input logic [SIZE-1:0] DI,
   input logic DSI,
   input logic nRST,
   input logic CLK,
@@ -6,13 +6,41 @@ module MEDIAN #(parameter SIZE = 8) (input logic [SIZE-1:0] DI,
   output logic DSO
   );
 
-  localparam I_WIDTH = $clog2(SIZE);
+  localparam I_WIDTH = $clog2(LENGHT);
 
-  logic [I_WIDTH-1:0] count;
+  logic [I_WIDTH-1:0] count, state;
+  logic oldDSI;
+
 
   always_ff @(posedge CLK)
+  if(!nRST)
   begin
-    count++;
+    count <= 0;
+    state <= 0;
   end
+  else
+  begin
+    oldDSI <= DSI;
+    if(oldDSI != DSI)
+    begin
+      count = 0;
+      state = 0;
+    end
+    if(!DSI)
+    begin
+      count = count + 1;
+      state <= (count == LENGHT) ? state+1 : state;
+      count <= (count == LENGHT) ? 0 : count;
+      if(state == 4 && count == 4)
+      begin
+        count <= 0;
+        state <= 0;
+      end
+    end
+  end
+
+  assign BYP = (count >= LENGHT - 1 - state);
+
+  MED I_MED(.DI(DI), .DSI(DSI), .BYP(BYP), .CLK(CLK), .DO(DO));
 
 endmodule
