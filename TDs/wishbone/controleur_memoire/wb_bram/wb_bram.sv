238a81e65feaa8e9c5e6e9e59cf5f00fae1a89f7
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2019-12-13 18:10:21 +0100

    Simulation du mode burst OK

--- TDs/wishbone/controleur_memoire/wb_bram/wb_bram.sv
+++ TDs/wishbone/controleur_memoire/wb_bram/wb_bram.sv
@@ -5,6 +5,7 @@
 // Le paramètre mem_adr_width doit permettre de déterminer le nombre
 // de mots de la mémoire : (2048 pour mem_adr_width=11)
 
+
 module wb_bram #(parameter mem_adr_width = 11) (
   // Wishbone interface
   wshb_if.slave wb_s
@@ -13,22 +14,39 @@ module wb_bram #(parameter mem_adr_width = 11) (
   /* PARTIE GENERATION DU ACK */
 
   // Compteur pour retarder l'ack
-  logic cnt;
+  logic counter, burst_enabled, burst_condition;
 
-  //Gestion du compteur
+  assign burst_condition = !counter && wb_s.cti[0] ^ wb_s.cti[1];
+
+  //Gestion du compteur et du burst
   always_ff @(posedge wb_s.clk)
   if(wb_s.rst)
-    cnt <= 0;
+  begin
+    counter <= 0;
+    burst_enabled <= 0;
+  end
   else
+  begin
     if(wb_s.stb)
-      cnt <= wb_s.we ? 0 : cnt+1;
+    begin
+      counter <= wb_s.we ^ burst_enabled ? 0 : counter+1;
+
+      // En supposant que le maître n'envoie pas de CTI "reserved"
+      if(burst_condition)
+        burst_enabled <= !wb_s.we;
+      if(wb_s.cti[2])
+        burst_enabled <= 0;
+    end
+    else
+      counter <= 0;
+  end
 
   //Gestion des signaux de sortie
   always_comb begin
     wb_s.rty = 0;
     wb_s.err = 0;
     if(wb_s.stb)
-      wb_s.ack = wb_s.we ? 1 : (cnt == 1);
+      wb_s.ack = wb_s.we ? 1 : (counter == 1) | burst_enabled;
     else
       wb_s.ack = 0;
   end
@@ -40,14 +58,7 @@ module wb_bram #(parameter mem_adr_width = 11) (
   logic[31:0] mask;
 
   //Redimensionnement addresse
-  assign adr = wb_s.adr[mem_adr_width-1:0] >> 2;
-
-  //Creation du masque
-  always_comb
-  begin
-    for(int k = 3; k >= 0 ; k--)
-      mask = {mask, {8{wb_s.sel[k]}}};
-  end
+  assign adr = (burst_enabled) ? (wb_s.adr >> 2 ) + 1 : wb_s.adr >> 2;
 
   //Ecriture et lecture mémoire
   always_ff @(posedge wb_s.clk)
@@ -55,7 +66,9 @@ module wb_bram #(parameter mem_adr_width = 11) (
     if (wb_s.we)
       for(int i = 1; i<=4; i++)
         if(wb_s.sel[i-1]) mem[adr][8*i - 1 -: 8] = wb_s.dat_ms[8*i - 1 -: 8];
-    wb_s.dat_sm <= mem[adr];
+          wb_s.dat_sm <= mem[adr];
   end
 
 endmodule
+
+// TODO : CORRECTION ADDRESSAGE MEMOIRE TAG 1 + REMISE DU CNT A 0
