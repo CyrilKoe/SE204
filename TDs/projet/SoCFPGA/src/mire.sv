7d61d34ccaacb013a34e3a15da212fd80a5a8105
Cyril KOENIG <cyril.koenig@telecom-paristech.fr>
2020-01-28 17:48:07 +0100

    Fin partie 4

--- TDs/projet/SoCFPGA/src/mire.sv
+++ TDs/projet/SoCFPGA/src/mire.sv
@@ -3,12 +3,9 @@ module mire #(parameter HDISP = 800, parameter VDISP = 480) (
 
 );
 
-localparam XCNT_WIDTH = $clog2(HDISP);
-localparam YCNT_WIDTH = $clog2(VDISP);
-logic[XCNT_WIDTH-1:0] x_cnt;
-logic[YCNT_WIDTH-1:0] y_cnt;
 logic[5:0] breaker;
-logic[15:0] img;
+
+////////// FAIR PLAY //////////
 
 always_ff @ (posedge wshb_ifm.clk)
 if(wshb_ifm.rst)
@@ -16,6 +13,14 @@ if(wshb_ifm.rst)
 else
   if(wshb_ifm.ack || (breaker == 0))
 			breaker <= breaker+1;
+
+
+////////// COMPTEURS GRAPHIQUES //////////
+
+localparam XCNT_WIDTH = $clog2(HDISP);
+localparam YCNT_WIDTH = $clog2(VDISP);
+logic[XCNT_WIDTH-1:0] x_cnt;
+logic[YCNT_WIDTH-1:0] y_cnt;
 
 always_ff @ (posedge wshb_ifm.clk)
 if(wshb_ifm.rst)
@@ -34,10 +39,11 @@ begin
 			begin
 				x_cnt <= 0;
 				y_cnt <= (y_cnt == VDISP-1) ? 0 : y_cnt+1;
-				if(y_cnt == VDISP-1) img++;
 			end
 		end
 end
+
+////////// SIGNAUX D'ECRITURE /////////
 
 assign wshb_ifm.stb = (breaker != 0) ;
 assign wshb_ifm.adr = (x_cnt + y_cnt*HDISP)*4;
@@ -46,6 +52,6 @@ assign wshb_ifm.cyc = wshb_ifm.stb;
 assign wshb_ifm.sel = 4'b1111;
 assign wshb_ifm.cti = '0;
 assign wshb_ifm.bte = '0;
-assign wshb_ifm.dat_ms = (x_cnt[4:0] == 0) || (y_cnt[4:0] == 0) ? '1 : {8'(x_cnt+y_cnt+img[12 -: 3]),(7'(x_cnt+y_cnt))<<1,~(8'(x_cnt+y_cnt))};
+assign wshb_ifm.dat_ms = (x_cnt[4:0] == 0) || (y_cnt[4:0] == 0) ? '1 : {8'(x_cnt+y_cnt),(7'(x_cnt+y_cnt))<<1,~(8'(x_cnt+y_cnt))};
 
 endmodule
